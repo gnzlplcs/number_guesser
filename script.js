6e58acb16955cd70d745f4777bbe138e63b7fbbd
gnzlplcs <palaciosgm@outlook.com>
2021-08-31 10:21:23 -0500

    assignment done

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-const generateTarget = () => {
-  return Math.floor(Math.random() * 10);
-}
+
+// this function generates a random number between 0 and 9
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+// this function compares the user guess and the computer random number
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  let humanDiff = Math.abs(targetGuess - humanGuess);
+  let computerDiff = Math.abs(targetGuess - computerGuess);
+  if (humanDiff <= computerDiff) {
+    return true;
+  }
+  return false;
+};
+
+// this function updates the current score
+const updateScore = str => str === 'human' ? humanScore++ : computerScore++;
+
+// this function increases the round
+const advanceRound = () => currentRoundNumber++;
